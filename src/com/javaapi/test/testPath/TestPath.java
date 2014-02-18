4184c8a4c09263a92c8d87f9030f1d6b400a6976
incoding <393630057@163.com>
2014-02-18 17:49:42 +0800

    changeName

--- src/com/javaapi/test/testPath/TestPath.java
+++ src/com/javaapi/test/testPath/TestPath.java
@@ -1,12 +1,13 @@
 package com.javaapi.test.testPath;
 
-public class Test {
+
+public class TestPath {
 public static void main(String[] args) {
 	ClassLoader cls=ClassLoader.getSystemClassLoader();
-	ClassLoader classCls=Test.class.getClassLoader();
+	ClassLoader classCls=TestPath.class.getClassLoader();
 	ClassLoader threadCls=Thread.currentThread().getContextClassLoader();
 	System.out.println(ClassLoader.getSystemResource("").getPath());
-	System.out.println(Test.class.getClassLoader().getResource("").getPath());
+	System.out.println(TestPath.class.getClassLoader().getResource("").getPath());
 	System.out.println(Thread.currentThread().getContextClassLoader().getResource("").getPath());
 	System.out.println(cls==classCls);
 	System.out.println(cls==threadCls);
