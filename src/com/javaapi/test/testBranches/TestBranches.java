b66c7533e61dbcdfe480166771bf46ef7adbcc60
incoding <393630057@163.com>
2014-02-23 14:36:13 +0800

    polishing

--- src/com/javaapi/test/testBranches/TestBranches.java
+++ src/com/javaapi/test/testBranches/TestBranches.java
@@ -187,30 +187,11 @@ public class TestBranches {
     @Test
     public void getName() {
         String paramone = "one";
-        String paramtwo = "one";
-        String paramthree = "one";
-        String paramfour = "one";
-        String paramfive = "one";
         Map<String, VarOne> mapOne = new HashMap<String, VarOne>();
-        // Map<String, VarThree> mapThree = new HashMap<String, VarThree>();
-        // Map<String, VarFour> mapFour = new HashMap<String, VarFour>();
-        // Map<String, VarFive> mapFive = new HashMap<String, VarFive>();
         // // 区分下一个维度的字段,可以通过set方法或者构造函数传入成员变量,也可以通过方法参数传入.方式不限
         System.out.println("维度---------1");
         mapOne.put("one", new VarOneImp1());
         mapOne.put("two", new VarOneImp2());
-        //
-        // System.out.println("维度---------3");
-        // mapThree.put("one", new VarThreeImp1());
-        // mapThree.put("two", new VarThreeImp2());
-        //
-        // System.out.println("维度---------4");
-        // mapFour.put("one", new VarFourImp1());
-        // mapFour.put("two", new VarFourImp2());
-        //
-        // System.out.println("维度---------5");
-        // mapFive.put("one", new VarFiveImp1());
-        // mapFive.put("two", new VarFiveImp2());
 
         mapOne.get(paramone).getName(paramone);
 
