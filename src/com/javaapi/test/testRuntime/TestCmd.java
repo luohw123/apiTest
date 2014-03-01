c4c113a279278eaee609ce8cc4cbcb994d2d2efc
incoding <393630057@163.com>
2014-03-01 17:36:38 +0800

    add test

--- src/com/javaapi/test/testRuntime/TestCmd.java
+++ src/com/javaapi/test/testRuntime/TestCmd.java
@@ -1,20 +1,27 @@
 package com.javaapi.test.testRuntime;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TestCmd {
+
     public TestCmd() {
     }
 
     public static void main(String args[]) {
         try {
             // 使用用Ping命令
-            String catlinahome = "F:\\Program Files\\apache-tomcat-6.0.37";
-            String file = catlinahome + "\\bin\\startup.bat";
+            // String catlinahome = "F:\\Program Files\\apache-tomcat-6.0.37";
+            // String file = catlinahome + "\\bin\\startup.bat";
             // String command2 = "cmd /c start " + file.replaceAll(" ",
             // "\" \"");
             // String command1 = "cmd  /c set 'CATALINA_HOME=" + catlinahome +
@@ -35,6 +42,30 @@ public class TestCmd {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void uploadFile(File file, String toFilePath)
+            throws IOException {
+        InputStream is = null;
+        BufferedOutputStream os = null;
+        try {
+            is = new FileInputStream(file);
+            os = new BufferedOutputStream(new FileOutputStream(toFilePath));
+            int bytesRead = 0;
+            byte[] buffer = new byte[8192];
+            while ((bytesRead = is.read(buffer, 0, 8192)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+        } catch (FileNotFoundException fnfe) {
+            throw fnfe;
+        } catch (IOException ioe) {
+            throw ioe;
+        } finally {
+            if (os != null)
+                os.close();
+            if (is != null)
+                is.close();
         }
     }
 }
