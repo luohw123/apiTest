a4fccb61d6c07833b879ad908681f5e484ab4f41
incoding <393630057@163.com>
2014-04-16 08:01:37 +0800

    add spring test comments

--- src/com/javaapi/test/spring/JunitSpring/TestJunitSpring.java
+++ src/com/javaapi/test/spring/JunitSpring/TestJunitSpring.java
@@ -7,6 +7,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
+/**
+ * 1 避免硬编码getBean</br>
+ * 2 不会进行多次spring容器初始化-速度加快</br>
+ * 3 数据库现场容易遭受破坏   ???</br>
+ * 4 没有对数据操作正确性进行检查   ???</br>
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("applicationContext.xml")
 public class TestJunitSpring {
