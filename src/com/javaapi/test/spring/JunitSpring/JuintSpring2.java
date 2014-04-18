aea723a6e90b5682cdbe9d9b28399131d669e458
incoding <393630057@163.com>
2014-04-18 08:06:03 +0800

    add the other method to make spring transactional available

--- src/com/javaapi/test/spring/JunitSpring/JuintSpring2.java
+++ src/com/javaapi/test/spring/JunitSpring/JuintSpring2.java
@@ -10,6 +10,9 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
  *1 支持事务回滚</br>
  *2 支持数据库操作,从而对数据正确性进行验证</br>
  *
+ *或者对 普通测试类进行 @Transactional 注解http://www.ibm.com/developerworks/cn/java/j-lo-springunitest/</br>
+ *   @Transactional 注释标签是表明此测试类的事务启用，这样所有的测试方案都会自动的 rollback，即您不用自己清除自己所做的任何对数据库的变更了
+ *
  */
 @ContextConfiguration("applicationContext.xml")
 public class JuintSpring2 extends AbstractTransactionalJUnit4SpringContextTests{
@@ -18,7 +21,6 @@ public class JuintSpring2 extends AbstractTransactionalJUnit4SpringContextTests{
 	
 	@Test
 	public void test(){
-		
 		school.haveClass();
 	}
 }
