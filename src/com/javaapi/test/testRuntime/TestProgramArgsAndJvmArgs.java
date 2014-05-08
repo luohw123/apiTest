d583dacc624961885e42e27ce2cb678396dc41bc
incoding <393630057@163.com>
2014-05-08 16:45:36 +0800

    add test

--- src/com/javaapi/test/testRuntime/TestProgramArgsAndJvmArgs.java
+++ src/com/javaapi/test/testRuntime/TestProgramArgsAndJvmArgs.java
@@ -13,8 +13,8 @@ public class TestProgramArgsAndJvmArgs {
 		}
 //       -Dproperty=value
 //		 设置系统属性名/值对，运行在此jvm之上的应用程序可用System.getProperty("property")得到value的值。
-		System.out.println(System.getenv("kkvalue"));
+		System.out.println(System.getenv("kkvalue")); //不能这样获取
 		System.out.println(System.getenv("sss"));
-		System.out.println(System.getProperty("sss"));
+		System.out.println(System.getProperty("sss"));// 要这样获取
 	}
 }
