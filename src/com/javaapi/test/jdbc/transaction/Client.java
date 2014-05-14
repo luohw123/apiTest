c0d4fc35b3167fea27631c822826106cbc97e31c
incoding <393630057@163.com>
2014-05-14 21:55:32 +0800

    add transactiontemplate test

--- src/com/javaapi/test/jdbc/transaction/Client.java
+++ src/com/javaapi/test/jdbc/transaction/Client.java
@@ -18,6 +18,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import com.javaapi.test.jdbc.JdbcExe;
 import com.javaapi.test.jdbc.JdbcHelper;
@@ -46,6 +48,13 @@ public class Client {
 	}
 
 	/**
+	 * 通过如上方式包装数据源后，可以在项目中使用物理事务编码的方式来获得逻辑事务的支持，即支持直接从DataSource获取连接和释放连接，且这些连接自动支持Spring逻辑事务； ???
+	 */
+	@Test
+	public void test(){
+		
+	}
+	/**
 	 * 
 	 * 模板类：使用Spring提供的模板类，如JdbcTemplate、HibernateTemplate和JpaTemplate模板类等，
 	 * 而这些模板类内部其实是使用了低级别解决方案中的工具类来管理连接或会话；</br> Spring提供两种编程式事务支持：
@@ -71,7 +80,8 @@ public class Client {
 		dataSourceTransactionManager.commit(status);
 	}
 	/**
-	 * 这种调用方式也是支持事物的
+	 * 高级别的解决方式:这种调用方式也是支持事物的
+	 * 如果采用编程式事务推荐使用TransactionTemplate模板类和高级别解决方案。
 	 */
 	@Test
 	public void testProgramTransactionRollBack() {
@@ -82,6 +92,43 @@ public class Client {
 		 System.out.println(result);
 		 dataSourceTransactionManager.rollback(status);
 //		dataSourceTransactionManager.commit(status);
+	}
+	/**使用TransactionTemplate模板类</br>
+	 * 如果采用编程式事务推荐使用TransactionTemplate模板类和高级别解决方案。
+	 * http://sishuok.com/forum/blogPost/list/2506.html
+	 * 如果发生异常需要自己处理并且显示调用status.setRollbackOnly() 后,模板才能在回调中回滚
+	 */
+	@Test
+	public void testTransactionTemplate(){
+		TransactionTemplate template=new TransactionTemplate(dataSourceTransactionManager);
+		int result = template.execute(new TransactionCallback<Integer>() {
+			@Override
+			public Integer doInTransaction(TransactionStatus status) {
+//				try{
+//					
+//				}catch(Exception e){
+//					// 需要显示回滚
+//					status.setRollbackOnly();
+//				}
+//				String bet007_id = "595959";
+//				String sql = "update matchs set league_name='斯伐乙西1'  where bet007_id="+bet007_id;
+				String id = "1";
+				String sql = "update csc_sns_dev.tbl_b set val='kk'  where id="+id;
+				int result=jdbcTemplate.update(sql);
+				System.out.println(result);
+				//如果需要回滚,则 需要显示回滚
+				boolean needRollBack = true;
+				// 没什么特别之处,
+				if(needRollBack){
+					status.setRollbackOnly();
+					return 0;
+				}else{
+					return result;
+				}
+				
+			}
+		});
+		System.out.println(result);
 	}
 	/**
 	 * 使用工具类获取连接（会话）和释放连接（会话），如使用org.springframework.jdbc.
@@ -96,6 +143,9 @@ public class Client {
 	 * http://sishuok.com/forum/blogPost/list/2506.html (好)
 	 * spring jdbc 编程事务
 	 * 
+	 */
+	/**
+	 * 低级别的解决方式
 	 */
 	@Test
 	public void testDataSourceTransaction() {
