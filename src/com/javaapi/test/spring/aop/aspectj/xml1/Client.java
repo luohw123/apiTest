e34886741f44ef5e3f2503fc0754d0341cd8ae72
incoding <393630057@163.com>
2014-05-17 21:25:35 +0800

    complete spring aop

--- src/com/javaapi/test/spring/aop/aspectj/xml1/Client.java
+++ src/com/javaapi/test/spring/aop/aspectj/xml1/Client.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * aspectj +spring ,xml
- *
+ *Spring + AspectJ（基于配置）
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("applicationContext.xml")
