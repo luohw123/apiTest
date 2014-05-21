fc0d9a079dfe2f27ef1be5c31e23822b6ecd7ba8
incoding <393630057@163.com>
2014-05-21 19:29:44 +0800

    add spring hessian  test

--- src/com/javaapi/test/spring/annotationXmlSpring/SpringTest.java
+++ src/com/javaapi/test/spring/annotationXmlSpring/SpringTest.java
@@ -5,8 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
- * http://pingping0524.iteye.com/blog/262482
- *
+ * http://pingping0524.iteye.com/blog/262482 取名字小写也可以
  */
 public class SpringTest {
 	@Test
