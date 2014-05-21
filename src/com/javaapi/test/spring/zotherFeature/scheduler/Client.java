9e0fa5c456620286b06afb487b841f0d84d18d56
incoding <393630057@163.com>
2014-05-21 14:39:48 +0800

    add spring cron scheduler

---
+++ src/com/javaapi/test/spring/zotherFeature/scheduler/Client.java
@@ -0,0 +1,24 @@
+package com.javaapi.test.spring.zotherFeature.scheduler;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("applicationContext.xml")
+public class Client {
+
+	@Test
+	public void test() {
+		try {
+			// 休眠十分钟
+			TimeUnit.HOURS.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/com/javaapi/test/spring/zotherFeature/scheduler/TaskJob.java
@@ -0,0 +1,11 @@
+package com.javaapi.test.spring.zotherFeature.scheduler;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskJob {
+
+	public void job1() {
+		System.out.println("任务执行");
+    }
+}
