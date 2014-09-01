a9e72f297a8ce52efec860c339c01cb43fb489ea
incoding <393630057@163.com>
2014-09-01 15:10:33 +0800

    add ThreadParamUtil

--- src/main/java/com/javaapi/test/concurrent/ThreadLocal/ThreadParamUtil.java
+++ src/main/java/com/javaapi/test/concurrent/ThreadLocal/ThreadParamUtil.java
@@ -4,9 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ThreadParamUtil {
-    private static ThreadLocal<Map<String, Object>> local = new ThreadLocal<>();
+    private static ThreadLocal<Map<String, Object>> local = new ThreadLocal<Map<String, Object>>();
 
     public static Map<String, Object> getLocal() {
+        if (local.get() == null) {
+            HashMap<String, Object> map = new HashMap<>();
+            local.set(map);
+        }
         return local.get();
     }
 
@@ -14,4 +18,20 @@ public class ThreadParamUtil {
         local.set(map);
     }
 
+    public static <T> void setLocalMap(String key, T value) {
+        if (local.get() == null) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            local.set(map);
+        }
+        local.get().put(key, (T) value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> T getLocalMap(String key) {
+        if (local.get() == null) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            local.set(map);
+        }
+        return (T) local.get().get(key);
+    }
 }
