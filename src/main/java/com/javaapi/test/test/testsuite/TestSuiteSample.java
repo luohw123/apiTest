912dc65a7ff515fbf0b74dc97361e5ffc7c6c2fa
incoding <393630057@163.com>
2014-10-06 22:27:53 +0800

    add test description

--- src/main/java/com/javaapi/test/test/testsuite/TestSuiteSample.java
+++ src/main/java/com/javaapi/test/test/testsuite/TestSuiteSample.java
@@ -8,6 +8,11 @@ import com.javaapi.test.spring.springioc.annotationSpring.Client;
 import com.javaapi.test.test.testmockito.MockitoSampleTest;
 import com.javaapi.test.test.testmockito.TestMockito;
 
+/**
+ * 这是junit得一系列文章</br>
+ * http://blog.csdn.net/luanlouis/article/details/37564355</br>
+ * 
+ */
 @RunWith(Suite.class)
 @SuiteClasses(value = { TestMockito.class, MockitoSampleTest.class,
 		Client.class })
