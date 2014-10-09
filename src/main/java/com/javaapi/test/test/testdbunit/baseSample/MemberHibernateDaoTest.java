bc7e0aca3146e7f6fdb81fb6aae34a09e7b92ff4
incoding <393630057@163.com>
2014-10-09 08:02:27 +0800

    add dbunit test

--- src/main/java/com/javaapi/test/test/testdbunit/baseSample/MemberHibernateDaoTest.java
+++ src/main/java/com/javaapi/test/test/testdbunit/baseSample/MemberHibernateDaoTest.java
@@ -13,6 +13,13 @@ import org.dbunit.operation.DatabaseOperation;
 /**
  * http://blog.csdn.net/abing37/article/details/5019882</br>
  * 会按着memberSet里的数据初始化数据,也就是数据库里原有的数据都会被干掉</br> h2,hsqldb</br>
+ * <p>
+ * http://www.ltesting.net/ceshi/open/kydycsgj/dbunit/2007/0622/63561.html</br>
+ * 　DBUnit框架提供了一个基本的抽象测试用例类，叫做DatabaseTestCase，它是JUnit框架中的基础类TestCase的子类。
+ * 如果我们使用这个类必须首先实现两个钩子方法(hook 　　methods)：getConnection()和getDataSet(). 　　
+ * 　　方法getConnection()需要返回一个IDatabaseConnection类型的对象，这个对象是一个基于普通JDBC连接的包装类。例如，
+ * 下面的代码段演示了在MySQL数据库环境下，IDatabaseConnection类型连接对象的创建方法。
+ * </p>
  * 
  * 
  */
@@ -34,11 +41,17 @@ public class MemberHibernateDaoTest extends DBTestCase {
 				"root");
 	}
  
-         @Override
+	/**
+	 * 执行测试之前对数据库数据集的操作
+	 */
+	@Override
          protected DatabaseOperation getSetUpOperation() throws Exception {
                    return DatabaseOperation.CLEAN_INSERT;
          }
- 
+
+	/**
+	 * 执行测试之后对数据库数据集的操作
+	 */
          @Override
          protected DatabaseOperation getTearDownOperation() throws Exception {
                    return DatabaseOperation.NONE;
