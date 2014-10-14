dca05037f9b5abaf4ba94df3d57591bf5c576375
incoding <393630057@163.com>
2014-10-15 03:58:36 +0800

    add spring test

---
+++ src/main/java/com/javaapi/test/spring/aop/aspectj/annotation/ClientPlain.java
@@ -0,0 +1,27 @@
+package com.javaapi.test.spring.aop.aspectj.annotation;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.javaapi.test.spring.aop.aspectj.CustomerBo;
+
+/**
+ * http://pingping0524.iteye.com/blog/262482
+ *
+ */
+/**1 父类依赖得静态代码块,父类依赖得实例代码块</br>2 父类静态代码块,父类实例代码块</br>
+ * 3 调用类得静态代码块,调用类的实例代码块 </br>  4 直接依赖的静态代码块,直接依赖的实例代码块</br>
+ * @author wk
+ *
+ */
+public class ClientPlain {
+	@Test
+	public void test() {
+		String path=ClientPlain.class.getResource("").getPath();
+		String filename = path+"applicationContext.xml";
+		ApplicationContext app=new FileSystemXmlApplicationContext(filename);
+		CustomerBo customerBo = (CustomerBo) app.getBean("customerBoImp");
+		customerBo.addCustomer();
+	}
+}
