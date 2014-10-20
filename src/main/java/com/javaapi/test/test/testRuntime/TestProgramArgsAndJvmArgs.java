4f7a2a95b1457eb554e2889863e308d1b800f101
incoding <393630057@163.com>
2014-10-20 22:21:31 +0800

    add jvm property test

--- src/main/java/com/javaapi/test/test/testRuntime/TestProgramArgsAndJvmArgs.java
+++ src/main/java/com/javaapi/test/test/testRuntime/TestProgramArgsAndJvmArgs.java
@@ -12,6 +12,7 @@ public class TestProgramArgsAndJvmArgs {
 			System.out.println(string);
 		}
 		// -Dsss=sssvalue
+
 //		 设置系统属性名/值对，运行在此jvm之上的应用程序可用System.getProperty("property")得到value的值。
 		System.out.println(System.getenv("kkvalue")); //不能这样获取
 		System.out.println(System.getenv("sss"));
