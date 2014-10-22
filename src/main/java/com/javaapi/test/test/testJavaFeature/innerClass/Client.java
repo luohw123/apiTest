a8de22ecf0d8a8fe26d2948af84186bd863430c4
incoding <393630057@163.com>
2014-10-23 07:27:51 +0800

    test inner param

--- src/main/java/com/javaapi/test/test/testJavaFeature/innerClass/Client.java
+++ src/main/java/com/javaapi/test/test/testJavaFeature/innerClass/Client.java
@@ -8,6 +8,17 @@ import org.junit.Test;
  * 
  */
 public class Client {
+	/**
+	 * 如果方法参数过多,而且不想在外部包写个参数占用位置,可以直接写在类里
+	 */
+	@Test
+	public void testInnerParam() {
+		Client client = new Client();
+		ClientInnerPrivate clientInnerPrivate = new ClientInnerPrivate();
+		clientInnerPrivate.setName("123");
+		client.testMethodInnerParam(clientInnerPrivate);
+	}
+
 	@Test
 	public void testPrivate() {
 		ClientInnerPrivate clientInner = new ClientInnerPrivate();
@@ -22,6 +33,10 @@ public class Client {
 		clientInner.setName("testProtected");
 		System.out.println(clientInner.getName());
 		System.out.println(clientInner.name);
+	}
+
+	public void testMethodInnerParam(ClientInnerPrivate innerParam) {
+		System.out.println(innerParam.getName());
 	}
 
 	private class ClientInnerPrivate {
