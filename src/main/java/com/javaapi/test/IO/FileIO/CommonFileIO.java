fa06daeb3d27fa6192364272365d0a151da30152
incoding <393630057@163.com>
2014-10-28 21:08:05 +0800

    add io test

--- src/main/java/com/javaapi/test/IO/FileIO/CommonFileIO.java
+++ src/main/java/com/javaapi/test/IO/FileIO/CommonFileIO.java
@@ -30,9 +30,9 @@ public class CommonFileIO {
 			+ S + "JavaApiSample" + S + "src" + S + "com" + S + "javaapi" + S
 			+ "test" + S + "testRuntime" + S + "TestCmd.txt";
 	private static final String STRING = txtPath;
-//	private static final String imagePath = "C:" + S + "Users" + S + "hncw" + S
-//			+ "Desktop" + S;
-	private static final String imagePath = "/home/kk/program/" + S;
+	private static final String	imagePath	= "C:" + S + "Users" + S + "hncw"
+													+ S + "Desktop" + S;
+	// private static final String imagePath = "/home/kk/program/" + S;
 
 	private static final String IMAGEPATH = imagePath;
 
@@ -113,6 +113,67 @@ public class CommonFileIO {
 		}
 	}
 
+	/**
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * 1234567890<br>
+	 * utf-8下 \r\n是2个字节 输出118(可能是最后一个换行符号没写入)
+	 */
+	@Test
+	public void testReadWriteText() throws IOException {
+		InputStream is = null;
+		OutputStream os = null;
+		BufferedInputStream br = null;
+		BufferedOutputStream bo = null;
+		try {
+			is = new FileInputStream(new File(imagePath + "136.txt"));
+
+			br = new BufferedInputStream(is);
+
+			os = new FileOutputStream(new File(imagePath + random + ".txt"));
+
+			bo = new BufferedOutputStream(os);
+			int length = 0;
+			int totalLength = 0;
+			// 1234567890\r\n (长度是12)
+			byte[] buf = new byte[12];
+			// 与下面一样
+			while ((length = br.read(buf)) > 0) {
+				System.out.println(length);
+				System.out.println(new String(buf, 0, 4));
+				// 只向文件中输出1234
+				os.write(buf, 0, 4);
+			}
+			// 与上面一样
+			// while ((length = br.read(buf)) > 0) {
+			// System.out.println(length);
+			// System.out.println(new String(buf));
+			// totalLength += length;
+			// bo.write(buf);
+			// }
+			System.out.println("-----------");
+			System.out.println("total-->" + totalLength);
+		} catch (IOException io) {
+			throw io;
+		} finally {
+			if (br != null)
+				br.close();
+			if (is != null)
+				is.close();
+			if (bo != null)
+				bo.close();
+			if (os != null)
+				os.close();
+		}
+
+	}
 	/**
 	 * 读取,输出二进制文件到内存,比如说图片
 	 * 
