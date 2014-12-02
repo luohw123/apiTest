27f8d98fc0024383c6a8296237ce88890aa03841
incoding <393630057@163.com>
2014-12-03 07:56:20 +0800

    test create procedure

--- src/main/java/com/javaapi/test/dao/jdbc/TestCallPROCEDURE.java
+++ src/main/java/com/javaapi/test/dao/jdbc/TestCallPROCEDURE.java
@@ -27,7 +27,7 @@ public class TestCallPROCEDURE {
 		Connection con = JdbcPrepare.getConnection(url, username, password);
 		CallableStatement call = null;
 		ResultSet rs = null;
-		String sql = "call usp1(?);";
+		String sql = "call usp2(?);";
 		String sql2 = "select @x";
 		try {
 			call = con.prepareCall(sql);
@@ -44,5 +44,45 @@ public class TestCallPROCEDURE {
 		} finally {
 			JdbcAfter.close(con, call, rs);
 		}
+	}
+	/**
+	 * CREATE PROCEDURE usp1(IN p INT) SET @x = p ;
+	 */
+	@Test
+	public void testCreateProceduer() {
+		Connection con = JdbcPrepare.getConnection(url, username, password);
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		String sql = "CREATE PROCEDURE usp3(OUT  inoutParam INT) SET inoutParam = '12347'";
+		try {
+			ps = con.prepareStatement(sql);
+			int executeUpdate = ps.executeUpdate();
+			System.out.println(executeUpdate);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcAfter.close(con, ps, rs);
+		}
+	}
+	@Test
+	public void testInvokeOutParam() {
+		Connection con = JdbcPrepare.getConnection(url, username, password);
+		CallableStatement call = null;
+		ResultSet rs = null;
+		String sql = "call usp2(?);";
+		try {
+			call = con.prepareCall(sql);
+			call.registerOutParameter(1, java.sql.Types.VARCHAR);
+			rs = call.executeQuery();
+			while (rs.next()) {
+				String string = rs.getString(1);
+				System.out.println(string);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcAfter.close(con, call, rs);
+		}
+	
 	}
 }
