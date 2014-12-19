b8ac3a2b3cc9e4e29018a99b5d6989edf863f917
incoding <393630057@163.com>
2014-12-19 17:24:47 +0800

    add junit test suite

---
+++ src/main/java/com/javaapi/test/test/testjunit/suite/ATest.java
@@ -0,0 +1,10 @@
+package com.javaapi.test.test.testjunit.suite;
+
+import org.junit.Test;
+
+public class ATest {
+    @Test
+    public void 测试功能1() {
+        System.out.println("test success!");
+    }
+}
---
+++ src/main/java/com/javaapi/test/test/testjunit/suite/Client.java
@@ -0,0 +1,12 @@
+package com.javaapi.test.test.testjunit.suite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ATest.class,BTest.class})
+public class Client {
+
+}
