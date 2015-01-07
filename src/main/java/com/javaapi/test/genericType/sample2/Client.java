a7f4c1d9d9f1b32e85fc247afcf120664497c324
incoding <393630057@163.com>
2015-01-07 22:36:20 +0800

    add generic test

--- src/main/java/com/javaapi/test/genericType/sample2/Client.java
+++ src/main/java/com/javaapi/test/genericType/sample2/Client.java
@@ -1,10 +1,7 @@
 package com.javaapi.test.genericType.sample2;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.junit.Test;
 
@@ -21,35 +18,52 @@ public class Client {
 		}else {
 			map = getZcRace();
 		}
-//		dealRace(map);
-		preHand(map);
+		dealRace(map);
+		dealMultiRace(map);
 	}
-	 public <T extends Race> void preHand(Map<String,T> map) {
-		 // 获得这个泛型T 能干嘛?
+	 /**
+	 * 想要获取具体子类型还是必须得强制转换
+	 */
+	public <T extends Race> void dealMultiRace(Map<String,T> map) {
+		 // 获得这个泛型T 能干嘛?  T能当做父类型Race来用
 //		 Iterator<Entry<String, T>> iterator = map.entrySet().iterator();
 		 // 可以直接当做 父类型使用. 
-		 T t = null;
-		 t.getName();
+		 T t = map.get("1");
+		 System.out.println("父类型"+t.getName());
 		 // 怎么在需要用子类型得时候得时候转为字类型 ?
 		 // 如下?
 		 if(t instanceof ZcRace) {
 			 ZcRace zc = (ZcRace)t;
 		 }
-		 	// 取出可以直接按照父类型取出
-		 Race race = map.get("");
+		 if(t instanceof DcRace) {
+			 DcRace zc = (DcRace)t;
+			 System.out.println("强转为子类型使用"+zc.getBifen());
+		 }
 	 }
 	
 	private void dealRace(Map<String, ? extends Race> map) {
 		// 取出可以直接按照父类型取出
-		Race race = map.get("");
+		Race t = map.get("1");
+		System.out.println(t.getName());
+		 if(t instanceof ZcRace) {
+			 ZcRace zc = (ZcRace)t;
+		 }
+		 if(t instanceof DcRace) {
+			 DcRace dc = (DcRace)t;
+			 System.out.println("强转为子类型使用"+dc.getBifen());
+		 }
+	}
+	private void dealIteratorRace(Map<String, ? extends Race> map) {
 		// 为什么迭代不可以 ??
 		map.entrySet().iterator();
-		
 	}
-
 	public Map<String,DcRace> getDcRace(){
 		
 		HashMap<String, DcRace> hashMap = new HashMap<>();
+		DcRace value = new DcRace();
+		value.setName("dc");
+		value.setBifen("123");
+		hashMap.put("1", value);
 		return hashMap;
 	}
 	
