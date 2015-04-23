a8c8b43ec9663b5c2b7921615dc07fc1126c7dc7
incoding <393630057@163.com>
2015-04-23 22:51:05 +0800

    hibernate mapping

---
+++ src/main/java/com/javaapi/test/dao/hibernate/biz/relation/many2manydanxiang/TestHibernateRelation.java
@@ -0,0 +1,5 @@
+package com.javaapi.test.dao.hibernate.biz.relation.many2manydanxiang;
+
+public class TestHibernateRelation {
+
+}
---
+++ src/main/java/com/javaapi/test/dao/hibernate/biz/relation/many2manyshuangxiang/TestHibernateRelation.java
@@ -0,0 +1,5 @@
+package com.javaapi.test.dao.hibernate.biz.relation.many2manyshuangxiang;
+
+public class TestHibernateRelation {
+
+}
