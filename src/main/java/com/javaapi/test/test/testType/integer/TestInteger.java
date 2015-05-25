ca707364cdcdd7f0fab9856a0ed0e443606310c2
incoding <393630057@163.com>
2015-05-25 21:52:29 +0800

    add test

--- src/main/java/com/javaapi/test/test/testType/integer/TestInteger.java
+++ src/main/java/com/javaapi/test/test/testType/integer/TestInteger.java
@@ -19,9 +19,20 @@ public class TestInteger {
 		Integer b = Integer.valueOf(129);
 		System.out.println(a == b);
 	}
-
+	@Test
+	public void testInteger127() {
+		Integer aInteger = 127;
+		Integer bInteger = 127;
+		System.out.println(aInteger == bInteger);
+	}
+	@Test
+	public void testInteger128() {
+		Integer aInteger = 128;
+		Integer bInteger = 128;
+		System.out.println(aInteger == bInteger);
+	}
 	/**
-	 * 自动装箱是靠 Integer.valueOf()来实现</br>
+	 * 自动装箱是靠 Integer.valueOf()来实现,Integer.valueOf 之后是Integer对象,</br>
 	 * http://blog.csdn.net/ma451152002/article/details/9076793
 	 */
 	@Test
@@ -30,10 +41,20 @@ public class TestInteger {
 		Integer aInteger = value;
 		Integer bInteger = value;
 		System.out.println(aInteger == bInteger);
+		System.out.println("---------------");
 		Integer aValue = Integer.valueOf(value);
 		Integer bValue = Integer.valueOf(value);
 		System.out.println(aValue == bValue);
 		System.out.println("---------------");
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		int value2 = 129;
 		Integer abig = value2;
 		Integer bbig = value2;
@@ -42,10 +63,11 @@ public class TestInteger {
 		Integer a = Integer.valueOf(value2);
 		Integer b = Integer.valueOf(value2);
 		System.out.println(a == b);
+		System.out.println("---------------");
 	}
 
 	/**
-	 * 因为在对a操作时(a=a+1或者a++)，a重新创建了一个对象，而b对应的还是缓存里的100，所以输出的结果为
+	 * 因为在对a操作时(a=a+1或者a++)，a重新创建了一个对象，而b对应的还是缓存里的100，所以输出的结果为false
 	 */
 	@Test
 	public void testInteger3() {
