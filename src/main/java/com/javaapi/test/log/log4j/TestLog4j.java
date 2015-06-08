c2c0928d081469053ea233ffdc5bd6fc11fe6e36
Incoding <393630057@163.com>
2015-06-08 09:47:38 +0800

    add log4j test

--- src/main/java/com/javaapi/test/log/log4j/TestLog4j.java
+++ src/main/java/com/javaapi/test/log/log4j/TestLog4j.java
@@ -13,5 +13,30 @@ public class TestLog4j {
 		logger.info("this is log4j style");
 		logger.info("test replace in {}", "log4j");
 	}
-
+	
+	/**
+	 * 异常不在占位符中,无论多少个占位符都会打印堆栈
+	 */
+	@Test
+	public void testSlf4jException() {
+		logger.info("test replace in {}","exception", new RuntimeException("sss"));
+	}
+	/**
+	 * 异常在占位符中,1个占位符号可以正确执行
+	 */
+	@Test
+	public void testSlf4jException1() {
+		logger.info("test replace in {}", new RuntimeException("sss"));
+	}
+	/**
+	 * 异常在占位符中,2个占位符号以及以上,不会打印堆栈
+	 */
+	@Test
+	public void testSlf4jException2() {
+		logger.info("test replace in {} {}","exception", new RuntimeException("sss"));
+	}
+	@Test
+	public void testSlf4jException3() {
+	}
+	
 }
