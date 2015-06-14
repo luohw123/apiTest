b76bfedf48cadc55b10f6a220c401164708dcc08
incoding <393630057@163.com>
2015-06-14 19:47:18 +0800

    add test

--- src/main/java/com/javaapi/test/exception/bussiness/ActionClientBackUp.java
+++ src/main/java/com/javaapi/test/exception/bussiness/ActionClientBackUp.java
@@ -7,20 +7,37 @@ import org.slf4j.LoggerFactory;
 import com.javaapi.test.exception.bussiness.exception.BusinessException;
 import com.javaapi.test.exception.bussiness.exception.ErrorCode;
 
-public class ActionClient {
+public class ActionClientBackUp {
 
-	static Logger logger = LoggerFactory.getLogger(ActionClient.class);
+	static Logger logger = LoggerFactory.getLogger(ActionClientBackUp.class);
 	
 	@Test
 	public void testRetry() throws Exception {
 		try {
 			BusinessService.seriviceBusiness();
 		} catch (Exception e) {
-			if (!(e instanceof BusinessException)) {
+			if (e instanceof BusinessException) {
+				boolean isSuccess = false;
+				BusinessException bussiness = (BusinessException) e;
+				if(bussiness.getError().getIndex() == ErrorCode.needRetry3.getIndex()) {
+					logger.info(ErrorCode.needRetry3.getMsg());
+					for (int i = 0; i < 3; i++) {
+						isSuccess=BusinessService.seriviceBusiness();
+						if(isSuccess) {
+							break;
+						}
+					}
+				}
+				else if(bussiness.getError().getIndex() == ErrorCode.doNotRetry.getIndex()) {
+					logger.info("乐观锁或其他异常",e);
+				}
+				else if (bussiness.getError().getIndex() == ErrorCode.commonError.getIndex()) {
+					String msg = bussiness.getError().getMsg();
+					logger.info("一般错误,错误信息={}",msg,e);
+				}
+			}else  {
 				logger.info("系统发生内部错误",e);
-				return;
-			} 
-			
+			}
 		}
 		System.out.println("全部业务执行成功");
 	}
