703b395290c2008fc62fa30c9e93862de68a581a
incoding <393630057@163.com>
2015-07-02 22:31:03 +0800

    add optimistic lock test

--- src/main/java/com/javaapi/test/dao/mybatis/springInterface/optimisticLock/CashBookDao.java
+++ src/main/java/com/javaapi/test/dao/mybatis/springInterface/optimisticLock/CashBookDao.java
@@ -8,8 +8,19 @@ public interface CashBookDao  {
 	
 	int updateBalance(CashBook cashbook);
 	
+	
+	CashBook selectOne_2(CashBook cashBook);
+	
+	
+	
+	int updateBalance_2(CashBook cashbook);
+	
 	//以下悲观锁
 	CashBook selectOneForUpdate(CashBook cashBook);
 	
 	int update(CashBook cashBook);
+	
+	CashBook selectOneForUpdate_2(CashBook cashBook);
+	
+	int update_2(CashBook cashBook);
 }
--- src/main/java/com/javaapi/test/dao/mybatis/springInterface/optimisticLock/CashBookService.java
+++ src/main/java/com/javaapi/test/dao/mybatis/springInterface/optimisticLock/CashBookService.java
@@ -76,25 +76,25 @@ public class CashBookService {
 		// 模拟其他业务更新 2 
 		CashBook cash_2  = new CashBook();
 		cash_2.setAccountId(accountId);
-		CashBook seletOne_2 = sqlSessionTemplate.getMapper(CashBookDao.class).selectOne(cash_2);
+		CashBook seletOne_2 = sqlSessionTemplate.getMapper(CashBookDao.class).selectOne_2(cash_2);
 		seletOne_2.setBalance(seletOne_2.getBalance().add(money));
-		int result_2 = sqlSessionTemplate.getMapper(CashBookDao.class).updateBalance(seletOne_2);
+		int result_2 = sqlSessionTemplate.getMapper(CashBookDao.class).updateBalance_2(seletOne_2);
 		if(result_2 ==0 ) {
 			throw new BusinessException(ErrorCode.needRetry3);
 		}else {
 			logger.info("success thread ={}",Thread.currentThread().getId());
 		}
 		// 模拟其他业务更新 3 
-		CashBook cash_3  = new CashBook();
-		cash_3.setAccountId(accountId);
-		CashBook seletOne_3 = sqlSessionTemplate.getMapper(CashBookDao.class).selectOne(cash_3);
-		seletOne_3.setBalance(seletOne_3.getBalance().add(money));
-		int result_3 = sqlSessionTemplate.getMapper(CashBookDao.class).updateBalance(seletOne_3);
-		if(result_3 ==0 ) {
-			throw new BusinessException(ErrorCode.needRetry3);
-		}else {
-			logger.info("success thread ={}",Thread.currentThread().getId());
-		}
+//		CashBook cash_3  = new CashBook();
+//		cash_3.setAccountId(accountId);
+//		CashBook seletOne_3 = sqlSessionTemplate.getMapper(CashBookDao.class).selectOne(cash_3);
+//		seletOne_3.setBalance(seletOne_3.getBalance().add(money));
+//		int result_3 = sqlSessionTemplate.getMapper(CashBookDao.class).updateBalance(seletOne_3);
+//		if(result_3 ==0 ) {
+//			throw new BusinessException(ErrorCode.needRetry3);
+//		}else {
+//			logger.info("success thread ={}",Thread.currentThread().getId());
+//		}
 	}
 
 }
