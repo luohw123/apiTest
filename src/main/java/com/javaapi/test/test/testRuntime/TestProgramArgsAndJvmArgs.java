ebbefa08b84970b3c48defecd0f1c5b9cdf68aa8
Incoding <393630057@163.com>
2015-07-18 12:20:41 +0800

    add test

--- src/main/java/com/javaapi/test/test/testRuntime/TestProgramArgsAndJvmArgs.java
+++ src/main/java/com/javaapi/test/test/testRuntime/TestProgramArgsAndJvmArgs.java
@@ -1,5 +1,9 @@
 package com.javaapi.test.test.testRuntime;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * @see http://blog.csdn.net/zolalad/article/details/11519327
  *  jvm的-D参数要在主类名之前,程序参数要在主类之后
@@ -13,9 +17,17 @@ public class TestProgramArgsAndJvmArgs {
 		}
 		// -Dsss=sssvalue
 
-//		 设置系统属性名/值对，运行在此jvm之上的应用程序可用System.getProperty("property")得到value的值。
-		System.out.println(System.getenv("kkvalue")); //不能这样获取
-		System.out.println(System.getenv("sss"));
+//		 设置jvm系统属性名/值对，运行在此jvm之上的应用程序可用System.getProperty("property")得到value的值。
 		System.out.println(System.getProperty("sss"));// 要这样获取
+	}
+	
+	/**
+	 * 获取linux/widows 服务器环境变量 
+	 */
+	@Test
+	public void getLiunxEnvVal() throws Exception {
+		System.out.println(System.getenv("kkvalue")); 
+		System.out.println(System.getenv("sss"));
+		System.out.println(System.getenv("PATH"));
 	}
 }
