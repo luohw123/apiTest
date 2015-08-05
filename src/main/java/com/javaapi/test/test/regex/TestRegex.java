18bb7b4fbad721f3296d110069637478cb665395
Incoding <393630057@163.com>
2015-08-05 18:15:17 +0800

    add test

---
+++ src/main/java/com/javaapi/test/test/regex/TestRegex.java
@@ -0,0 +1,12 @@
+package com.javaapi.test.test.regex;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRegex {
+@Test
+public void test() throws Exception {
+	
+}
+}
