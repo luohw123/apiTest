51d2a587734a76be8e1ab3bcb0c026d03a438f74
Incoding <393630057@163.com>
2015-08-10 18:51:54 +0800

    add test

--- src/main/java/com/javaapi/test/test/map/collectionutil/Client.java
+++ src/main/java/com/javaapi/test/test/map/collectionutil/Client.java
@@ -1,16 +1,11 @@
 package com.javaapi.test.test.map.collectionutil;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.collections4.Transformer;
 import org.junit.Test;
-
-import com.google.common.base.Function;
-import com.google.common.collect.Maps;
 
 /**
  * http://www.ylzx8.cn/zonghe/open-source/248952.html</br>
@@ -39,4 +34,30 @@ public class Client {
 		}, category);
 		System.err.println(category);
 	}
+	/**
+	 *  获取List<Person> 里的 id 得list
+	 */
+	@Test
+	public void testGetIdList() throws Exception {
+		List<Book> list = new ArrayList<>();
+		Book book = new Book();
+		book.setId(1);
+		book.setCategory("nihao");
+		Book book2 = new Book();
+		book2.setId(2);
+		book2.setCategory("nihao2");
+		list.add(book);
+		list.add(book2);
+		List<String> category = new ArrayList<>();
+		CollectionUtils.collect(list, new Transformer<Book, String>() {
+			@Override
+			public String transform(Book input) {
+				return input.getCategory();
+			}
+		}, category);
+		System.err.println(category);
+	}
+//    public static Collection collect(Collection collection, String propertyName) {  
+//        return CollectionUtils.collect(collection, new BeanToPropertyValueTransformer(propertyName));  
+//    }  
 }
