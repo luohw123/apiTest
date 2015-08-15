0b73e3f9dc309b244c5887ec740f1c13ead0e2e6
wangkun <wangkun@acfun.tv>
2015-08-15 18:32:09 +0800

    test java8 Lambda

--- src/main/java/com/javaapi/test/test/testJavaFeature/Version_1_8/Client.java
+++ src/main/java/com/javaapi/test/test/testJavaFeature/Version_1_8/Client.java
@@ -31,4 +31,44 @@ public class Client {
         players.forEach(System.out::println);
     }
 
+    @Test
+    public void testRunnable(){
+        // 1.1使用匿名内部类
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello world !匿名内部类");
+            }
+        }).start();
+
+// 1.2使用 lambda expression
+        new Thread(() -> System.out.println("Hello world ! lambda")).start();
+
+// 2.1使用匿名内部类
+        Runnable race1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hello world ! 匿名内部类");
+            }
+        };
+
+// 2.2使用 lambda expression
+        Runnable race2 = () -> System.out.println("Hello world ! lambda表达式返回runnable");
+
+// 直接调用 run 方法(没开新线程哦!)
+        race1.run();
+        race2.run();
+    }
+
+    @Test
+    public void testMyself(){
+        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+                "Stanislas Wawrinka",
+                "David Ferrer","Roger Federer",
+                "Andy Murray","Tomas Berdych",
+                "Juan Martin Del Potro"};
+        List<String> players =  Arrays.asList(atp);
+        players.forEach((player)-> System.out.println(player));
+    }
+
 }
