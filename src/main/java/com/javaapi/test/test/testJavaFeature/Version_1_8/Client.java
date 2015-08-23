3f4587b592f4599d72cb576bdde68f333aaf3e42
incoding <393630057@163.com>
2015-08-23 11:01:26 +0800

    add comment

--- src/main/java/com/javaapi/test/test/testJavaFeature/Version_1_8/Client.java
+++ src/main/java/com/javaapi/test/test/testJavaFeature/Version_1_8/Client.java
@@ -9,66 +9,66 @@ import java.util.List;
  * Created by user on 15/8/15.
  */
 public class Client {
-
-    @Test
-    public void test(){
-        String[] atp = {"Rafael Nadal", "Novak Djokovic",
-                "Stanislas Wawrinka",
-                "David Ferrer","Roger Federer",
-                "Andy Murray","Tomas Berdych",
-                "Juan Martin Del Potro"};
-        List<String> players =  Arrays.asList(atp);
-
-// 以前的循环方式
-        for (String player : players) {
-            System.out.print(player + "; ");
-        }
-
-// 使用 lambda 表达式以及函数操作(functional operation)
-        players.forEach((player) -> System.out.print(player + "; "));
-
-// 在 Java 8 中使用双冒号操作符(double colon operator)
-        players.forEach(System.out::println);
-    }
-
-    @Test
-    public void testRunnable(){
-        // 1.1使用匿名内部类
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello world !匿名内部类");
-            }
-        }).start();
-
-// 1.2使用 lambda expression
-        new Thread(() -> System.out.println("Hello world ! lambda")).start();
-
-// 2.1使用匿名内部类
-        Runnable race1 = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Hello world ! 匿名内部类");
-            }
-        };
-
-// 2.2使用 lambda expression
-        Runnable race2 = () -> System.out.println("Hello world ! lambda表达式返回runnable");
-
-// 直接调用 run 方法(没开新线程哦!)
-        race1.run();
-        race2.run();
-    }
-
-    @Test
-    public void testMyself(){
-        String[] atp = {"Rafael Nadal", "Novak Djokovic",
-                "Stanislas Wawrinka",
-                "David Ferrer","Roger Federer",
-                "Andy Murray","Tomas Berdych",
-                "Juan Martin Del Potro"};
-        List<String> players =  Arrays.asList(atp);
-        players.forEach((player)-> System.out.println(player));
-    }
+//
+//    @Test
+//    public void test(){
+//        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+//                "Stanislas Wawrinka",
+//                "David Ferrer","Roger Federer",
+//                "Andy Murray","Tomas Berdych",
+//                "Juan Martin Del Potro"};
+//        List<String> players =  Arrays.asList(atp);
+//
+//// 以前的循环方式
+//        for (String player : players) {
+//            System.out.print(player + "; ");
+//        }
+//
+//// 使用 lambda 表达式以及函数操作(functional operation)
+//        players.forEach((player) -> System.out.print(player + "; "));
+//
+//// 在 Java 8 中使用双冒号操作符(double colon operator)
+//        players.forEach(System.out::println);
+//    }
+//
+//    @Test
+//    public void testRunnable(){
+//        // 1.1使用匿名内部类
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Hello world !匿名内部类");
+//            }
+//        }).start();
+//
+//// 1.2使用 lambda expression
+//        new Thread(() -> System.out.println("Hello world ! lambda")).start();
+//
+//// 2.1使用匿名内部类
+//        Runnable race1 = new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Hello world ! 匿名内部类");
+//            }
+//        };
+//
+//// 2.2使用 lambda expression
+//        Runnable race2 = () -> System.out.println("Hello world ! lambda表达式返回runnable");
+//
+//// 直接调用 run 方法(没开新线程哦!)
+//        race1.run();
+//        race2.run();
+//    }
+//
+//    @Test
+//    public void testMyself(){
+//        String[] atp = {"Rafael Nadal", "Novak Djokovic",
+//                "Stanislas Wawrinka",
+//                "David Ferrer","Roger Federer",
+//                "Andy Murray","Tomas Berdych",
+//                "Juan Martin Del Potro"};
+//        List<String> players =  Arrays.asList(atp);
+//        players.forEach((player)-> System.out.println(player));
+//    }
 
 }
