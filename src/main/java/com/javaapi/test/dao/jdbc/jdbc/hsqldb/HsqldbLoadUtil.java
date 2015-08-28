c3f2d545fb58b788f7f0c7a0764e1caa35c8cb2c
wangkun <wangkun@acfun.tv>
2015-08-28 10:01:36 +0800

    refactor hsqldbutil

--- src/main/java/com/javaapi/test/dao/jdbc/jdbc/hsqldb/HsqldbLoadUtil.java
+++ src/main/java/com/javaapi/test/dao/jdbc/jdbc/hsqldb/HsqldbLoadUtil.java
@@ -1,17 +1,12 @@
 package com.javaapi.test.dao.jdbc.jdbc.hsqldb;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
+import java.io.*;
 import java.sql.Connection;
 import java.util.List;
 import java.util.Map;
@@ -20,49 +15,63 @@ import java.util.Map;
  * Created by user on 15/8/27.
  */
 public class HsqldbLoadUtil {
-        private static SqlSessionFactory sqlSessionFactory;
+    private static SqlSessionFactory sqlSessionFactory;
 
-        static {
-            try {
-                //创建SqlSessionFactory
-                String resource = HsqldbLoadUtil.class.getResource("mybatis-java.xml").getPath();
-                String createSql = HsqldbLoadUtil.class.getResource("CreateDB.sql").getPath();
-                Reader reader = new InputStreamReader(new FileInputStream(resource));
-                sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-                reader.close();
-                //创建数据库
-                SqlSession session = null;
-                try {
-                    session = sqlSessionFactory.openSession();
+    static {
+        try {
+            //创建SqlSessionFactory
+            String resource = HsqldbLoadUtil.class.getResource("mybatis-java.xml").getPath();
+            Reader reader = new InputStreamReader(new FileInputStream(resource));
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-                    Connection conn = session.getConnection();
-                    reader = new InputStreamReader(new FileInputStream(createSql));
-                    ScriptRunner runner = new ScriptRunner(conn);
-                    runner.setLogWriter(null);
-                    runner.runScript(reader);
-                    reader.close();
-                } finally {
-                    if (session != null) {
-                        session.close();
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+    public static void loadScript(String absolutePath) {
+        //创建数据库
+        SqlSession session = null;
+        try {
+            Reader reader = null;
+            session = getSqlSession();
+            Connection conn = session.getConnection();
+            reader = new InputStreamReader(new FileInputStream(absolutePath));
+            ScriptRunner runner = new ScriptRunner(conn);
+            runner.setLogWriter(null);
+            runner.runScript(reader);
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
             }
         }
+    }
 
-        /**
-         * 获取Session
-         * @return
-         */
-        public static SqlSession getSqlSession(){
-            return sqlSessionFactory.openSession();
-        }
+    /**
+     * 获取Session
+     *
+     * @return
+     */
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+
     @Test
-    public void test() {
+    public void testSelect() {
         SqlSession sqlSession = getSqlSession();
-        List<Map<String,String>> list = sqlSession.selectList("testNamespace.select");
+        List<Map<String, String>> list = sqlSession.selectList("testNamespace.select");
         System.out.println("list = " + list);
     }
 
+    @Test
+    public void testLoadScript() throws Exception {
+        String resource = HsqldbLoadUtil.class.getResource("CreateDB.sql").getPath();
+        HsqldbLoadUtil.loadScript(resource);
+        testSelect();
+    }
 }
