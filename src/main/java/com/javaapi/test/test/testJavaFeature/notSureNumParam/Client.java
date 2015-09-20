6454fd619e892354f5fe401bfa00722fc979623d
wangkun <wangkun@acfun.tv>
2015-09-21 07:41:56 +0800

    add not sure param test

---
+++ src/main/java/com/javaapi/test/test/testJavaFeature/notSureNumParam/Client.java
@@ -0,0 +1,22 @@
+package com.javaapi.test.test.testJavaFeature.notSureNumParam;
+
+import org.junit.Test;
+
+/**
+ * Created by user on 15/9/21.
+ */
+public class Client {
+
+    @Test
+    public void test(){
+        Integer[] ids = new Integer[]{5,6,7};
+        notSureParam(1);
+        notSureParam(2,3,4);
+        notSureParam(ids);
+    }
+    public void notSureParam(Integer... ids){
+        for (Integer id : ids) {
+            System.out.println("id = " + id);
+        }
+    }
+}
---
+++ src/main/java/com/javaapi/test/test/testProxy/cglibEg2/cglib3/Client.java
@@ -0,0 +1,7 @@
+package com.javaapi.test.test.testProxy.cglibEg2.cglib3;
+
+/**
+ * http://isuifengfei.iteye.com/blog/1682606
+ */
+public class Client {
+}
