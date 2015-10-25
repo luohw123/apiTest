8d2f44077d6a7516855932efda27efb4cbd69e8c
wangkun <wangkun@acfun.tv>
2015-10-25 11:37:22 +0800

    add test

--- src/main/java/com/javaapi/test/shell/sample/curlwww.sh
+++ src/main/java/com/javaapi/test/shell/sample/curlwww.sh
@@ -2,6 +2,6 @@
 for((x=0;x<20;x++))
 do
 
-	curl   -sd "username=a&password=1234562&captcha=a&time=`date +%s%N`" "http://www.acfun.tv/login.aspx"  |xargs echo 'www.acfun.tv'
+	curl   -sd "username=a&password=1234562&captcha=a&time=`date +%s%N`" "http://www.baidu.com/login.aspx"  |xargs echo 'www.acfun.tv'
 	echo '-------'
 done
