0ace80d34252d7cb71b2489bf84672fb4ff6f5ff
wangkun <wangkun@acfun.tv>
2016-04-21 22:43:00 +0800

    test quartz usage

--- src/main/java/com/javaapi/test/spring/zotherFeature/scheduler/quartz/dynamicOriginAdd/QuartzJob.java
+++ src/main/java/com/javaapi/test/spring/zotherFeature/scheduler/quartz/dynamicOriginAdd/QuartzJob.java
@@ -4,6 +4,13 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 只要线程够就不会影响下次任务执行.如果所有任务都在执行中,则会等到空出线程再执行任务
+ *
+ * 另外可以设置quartz是否并发执行
+ */
 public class QuartzJob implements Job {
 
     @Override
@@ -13,5 +20,10 @@ public class QuartzJob implements Job {
 //        SelfJob object = (SelfJob) context.getMergedJobDataMap().get("selfJob");
         System.out.println("SelfJob回调SelfJob==>"+this);
 //		extracted();
+        try {
+            TimeUnit.SECONDS.sleep(30);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }  
