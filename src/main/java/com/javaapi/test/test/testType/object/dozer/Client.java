9294462fffa78c76e9833764bc2a68e0c589db5e
wangkun <wangkun@acfun.tv>
2016-08-13 21:24:53 +0800

    test object mapper

---
+++ src/main/java/com/javaapi/test/test/testType/object/dozer/Client.java
@@ -0,0 +1,8 @@
+package com.javaapi.test.test.testType.object.dozer;
+
+/**
+ * TODO test dozer
+ * object to dto  .etc
+ */
+public class Client {
+}
---
+++ src/main/java/com/javaapi/test/test/testType/object/orika/Client.java
@@ -0,0 +1,8 @@
+package com.javaapi.test.test.testType.object.orika;
+
+/**
+ * TODO test orika
+ * object to dto  .etc
+ */
+public class Client {
+}
