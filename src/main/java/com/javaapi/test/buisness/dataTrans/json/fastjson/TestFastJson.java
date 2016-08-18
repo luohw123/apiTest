da939bc9485eb3117330b52d430dcdedb73c4c5a
wangkun <wangkun@acfun.tv>
2016-08-18 22:08:15 +0800

    test fastjson

--- src/main/java/com/javaapi/test/buisness/dataTrans/json/fastjson/TestFastJson.java
+++ src/main/java/com/javaapi/test/buisness/dataTrans/json/fastjson/TestFastJson.java
@@ -1,24 +1,21 @@
 package com.javaapi.test.buisness.dataTrans.json.fastjson;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import jodd.io.FileUtil;
-
-import org.apache.commons.lang.CharEncoding;
-import org.apache.commons.lang.time.StopWatch;
-import org.junit.Test;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.alibaba.fastjson.parser.Feature;
-import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.util.TypeUtils;
+import jodd.io.FileUtil;
+import org.apache.commons.lang.CharEncoding;
+import org.apache.commons.lang.time.StopWatch;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 1.数组或对象的每个成员的值，可以是简单值，也可以是复合值。</br>
@@ -156,6 +153,7 @@ public class TestFastJson {
 		TypeUtils.compatibleWithJavaBean = true;
 		System.out.println(JSON.toJSONString(wo));
 	}
+
 	@Test
 	public void parseNull() throws Exception {
 		JSONObject parseObject = JSON.parseObject(null);
@@ -163,4 +161,17 @@ public class TestFastJson {
 		Boolean boolean1 = parseObject.getBoolean("isSuccess");
 		System.out.println(boolean1);
 	}
+
+    @Test
+    public void test() {
+        List<Map<String, String>> list = new ArrayList<>();
+        Map<String, String> map = new HashMap<>();
+        map.put("nihao", "nihao");
+        list.add(map);
+        list.add(new HashMap<>(map));
+        String s = JSON.toJSONString(list);
+        System.out.println("s = " + s);
+
+
+    }
 }
