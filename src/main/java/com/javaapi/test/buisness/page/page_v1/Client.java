32915abda31e16a0361caf58b164d1c9333e6d68
wangkun <wangkun@acfun.tv>
2016-11-20 12:49:49 +0800

    sync code

--- src/main/java/com/javaapi/test/buisness/page/page_v1/Client.java
+++ src/main/java/com/javaapi/test/buisness/page/page_v1/Client.java
@@ -3,6 +3,7 @@ package com.javaapi.test.buisness.page.page_v1;
 import org.junit.Test;
 
 /**
+ * 原先是不带泛型的版本,我改造了下改成了带泛型的版本
  * @see com.javaapi.test.dao.domain.page.Pagination
  */
 public class Client {
